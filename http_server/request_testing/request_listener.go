6a371438102c850b3af83f5d2439880ba2364f14
mm2260 <maramihali@google.com>
2020-07-03 11:18:24 +0300

    Resolved some of the comments and added tests

--- http_server/request_testing/request_listener.go
+++ http_server/request_testing/request_listener.go
@@ -1,27 +1,28 @@
 package requesttesting
 
 import (
-	"net"
-	"errors"
+	"io"
 	"log"
+	"net"
+	"sync"
 )
 
 type mockListener struct {
-	connChannel chan net.Conn
+	closeOnce      sync.Once
+	connChannel    chan net.Conn
 	serverEndpoint net.Conn
 }
 
-// For each request, we create an HTTP listener that passes the request provided
-// as a function parameter to the server.
-func newMockListener(endpoint  net.Conn) mockListener {
+// Creates a mock listener that passes requests to the HTTP server as part of
+// the test harness
+func newMockListener(endpoint net.Conn) *mockListener {
 
-	channel := make(chan net.Conn, 1)
-	channel <- endpoint
-	listener := mockListener{
-		connChannel: channel,
+	c := make(chan net.Conn, 1)
+	c <- endpoint
+	listener := &mockListener{
+		connChannel:    c,
 		serverEndpoint: endpoint,
 	}
-	
 	return listener
 }
 
@@ -29,26 +30,23 @@ func newMockListener(endpoint  net.Conn) mockListener {
 // Accept() and hence Close() are being called twice, but the handler only once.
 func (l *mockListener) Accept() (net.Conn, error) {
 	log.Print("Accept() called")
-	ch, ok := <- l.connChannel
+	ch, ok := <-l.connChannel
 	if !ok {
-		return nil, errors.New("EOF")
+		return nil, io.EOF
 	}
 	return ch, nil
 }
 
 func (l *mockListener) Close() error {
 	log.Print("Close() called")
-	// My intuition here would have been to close both the endpoint and the
-	// channel. However, if I do that, I trigger a panic of "close of closed
-	// channel" when I attempt to close the channel. If I only close the // //
-	// channel, the program never terminates (I
-	// assume the server continues listening)
-	l.serverEndpoint.Close()
-	// close(l.connChannel)
+	o := &l.closeOnce
+	o.Do(func() {
+		l.serverEndpoint.Close()
+		close(l.connChannel)
+	})
 	return nil
 }
 
 func (l *mockListener) Addr() net.Addr {
 	return l.serverEndpoint.LocalAddr()
 }
-
--- http_server/request_testing/request_test.go
+++ http_server/request_testing/request_test.go
@@ -1,48 +1,56 @@
 package requesttesting
 
 import (
+	"regexp"
 	"context"
 	"log"
 	"net"
 	"net/http"
 	"testing"
+	"strings"
 )
 
-type handlerRecorder struct {
-	req         *http.Request
-	finishedReq chan bool
-	//TODO(mara): add server response
+type recorderHandler struct {
+	req     *http.Request
+	doneReq chan bool
 }
 
-
-func (hr *handlerRecorder) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (hr *recorderHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	hr.req = r
 	log.Print("Handler was called.")
-	// log.Print(hr.req.URL.Query()["vegetable"])
-	hr.finishedReq <- true
+	hr.doneReq <- true
 }
 
-func makeRequest(req string) *http.Request {
+func makeRequest(req string) (reqReceived *http.Request, resp []byte) {
+
 	e1, e2 := net.Pipe()
 	listener := newMockListener(e2)
-	done := make(chan bool)
-	defer listener.Close()
-	handler := &handlerRecorder{req: nil, finishedReq: done}
+	// defer listener.Close()
+	handler := &recorderHandler{req: nil, doneReq: make(chan bool)}
 	server := &http.Server{Handler: handler}
-	go server.Serve(&listener)
+	go server.Serve(listener)
 	defer server.Shutdown(context.Background())
 	e1.Write([]byte(req))
-	<-handler.finishedReq
-	return handler.req
+	resp = make([]byte, 4096)
+	go func() {
+		e1.Read(resp)
+		// TODO(MARA) replace with something bettere
+		r, _ := regexp.Compile("HTTP/[0-9].[0-9] [4-5][0-9][0-9]")
+		if r.MatchString(string(resp)) {
+			handler.doneReq <- false
+		}
+	}()
+	<-handler.doneReq
+	reqReceived = handler.req
+	return
 }
 
-func TestSetup(t *testing.T) {
-	req := makeRequest("GET / HTTP/1.1\r\n" +
+// Tests the setup
+func TestMakeRequest(t *testing.T) {
+
+	req, _ := makeRequest("GET / HTTP/1.1\r\n" +
 		"Host: localhost:8080\r\n" + "\r\n")
 
-	if req == nil {
-		t.Error("Expected GET Request, received nil")
-	}
 	if req.Method != http.MethodGet {
 		t.Errorf("Expected %s, received %s", http.MethodGet, req.Method)
 	}
@@ -50,63 +58,95 @@ func TestSetup(t *testing.T) {
 
 // Ensures Query() only returns a map of size one and verifies whether sending a
 // request with two values for the same key eturns a []string of length 2
-// containing the correct values 
-func TestQMultipleValsSameKey(t *testing.T) {
-	valueOne := "potatO"
-	valueTwo := "Tomato"
-	reqString := "GET /?vegetable=" + valueOne + "&vegetable=" + valueTwo + " HTTP/1.1\r\n" + "Host: localhost:8080\r\n" + "\r\n"
-	log.Print(reqString)
-	req := makeRequest(reqString)
-
+// containing the correct values
+func TestMultipleQueryParametersSameKey(t *testing.T) {
+	var (
+		valueOne  = "potatO"
+		valueTwo  = "Tomato"
+		reqString = "GET /?vegetable=" + valueOne + "&vegetable=" + valueTwo + " HTTP/1.1\r\n" + "Host: localhost:8080\r\n" + "\r\n"
+	)
+	req, _ := makeRequest(reqString)
 	queryParams := req.URL.Query()
 	if len(queryParams) != 1 {
-		t.Errorf("Expected one query parameter to have been received, got %d", len(queryParams))
+		t.Errorf("len(queryParams): got %d, want %d", len(queryParams), 1)
 	}
 
 	vegetableParamValues := queryParams["vegetable"]
 	if len(vegetableParamValues) != 2 {
-		t.Errorf("Expected 2 query parameter values got %d", len(vegetableParamValues))
+		t.Errorf("len(vegetableQueryParams): got %d, want %d", len(vegetableParamValues), 2)
 	}
 	if vegetableParamValues[0] != valueOne || vegetableParamValues[1] != valueTwo {
-		t.Errorf("Expected "+valueOne+" and "+valueTwo+", got %s and %s", vegetableParamValues[0], vegetableParamValues[1])
+		t.Errorf("queryParams values: expected "+valueOne+" and "+valueTwo+", got %s and %s", vegetableParamValues[0], vegetableParamValues[1])
 	}
 
 }
 
 // Ensure different casing in  keys results in different query parameters
-func TestQKeysDifferentCasing(t *testing.T) {
-		req := makeRequest("GET /?vegetable=potato&Vegetable=tomato HTTP/1.1\r\n" +
+func TestQueryParametersSameKeyDifferentCasing(t *testing.T) {
+	req, _ := makeRequest("GET /?vegetable=potato&Vegetable=tomato HTTP/1.1\r\n" +
 		"Host: localhost:8080\r\n" + "\r\n")
-		queryParams := req.URL.Query()
-		if len(queryParams) != 2 {
-			t.Errorf("Expected 2 query parameters, got %d", len(queryParams))
-		}
+	queryParams := req.URL.Query()
+	if len(queryParams) != 2 {
+		t.Errorf("len(queryParams): got %d, want %d", len(queryParams), 2)
+	}
 
 	if len(queryParams["vegetable"]) != 1 || len(queryParams["Vegetable"]) != 1 {
 		t.Errorf("Expected one value for query parameter vegetables and one for Vegetables, got %d and %d", len(queryParams["vegetable"]), len(queryParams["Vegetable"]))
 	}
+	log.Print(queryParams["vegetable"][0])
 }
 
 // Ensure keys and values that contain non-ASCII characters are parsed correctly
-func TestQRomanianCharacters(t *testing.T) {
-  value := "ăȚâȘî"
-  reqString := "GET /?vegetable=" + value + " HTTP/1.1\r\n" + "Host: localhost:8080\r\n" + "\r\n"
-  req := makeRequest(reqString)
- 
-  if   valueReceived := req.URL.Query()["vegetable"][0]; valueReceived != value {
-	  t.Errorf("Expected %s as value but got %s", value, valueReceived)
-  }
+func TestQueryParametersValidUnicode(t *testing.T) {
+	value := "ăȚâȘî"
+	reqString := "GET /?vegetable=" + value + " HTTP/1.1\r\n" + "Host: localhost:8080\r\n" + "\r\n"
+	req, _ := makeRequest(reqString)
 
-  key := "ăȚâȘî"
-  reqString = "GET /?" + key + "=vegetable HTTP/1.1\r\n" + "Host: localhost:8080\r\n" + "\r\n"
-  req = makeRequest(reqString)
-  
-  if  listLen := len(req.URL.Query()[key]); listLen != 1 {
-	  t.Errorf("Expected 1 value for key %s but got %d", key, listLen)
-  }
+	if valueReceived := req.URL.Query()["vegetable"][0]; valueReceived != value {
+		t.Errorf("queryParams values: got %s, want %s", valueReceived, value)
+	}
+
+	key := "ăȚâȘî"
+	reqString = "GET /?" + key + "=vegetable HTTP/1.1\r\n" + "Host: localhost:8080\r\n" + "\r\n"
+	req, _ = makeRequest(reqString)
+
+	if listLen := len(req.URL.Query()[key]); listLen != 1 {
+		t.Errorf("len(queryParamsKey): got %d, want 1 value for %s", listLen, key)
+	}
+}
+
+// Tests whether passing invalid Unicode will result  in a 400 Bad Request error
+func TestQueryParametersInvalidUnicodes(t *testing.T) {
+	keyOne := "\x0F"
+	reqString := "GET /?" + keyOne + "=tomato&Vegetable=potato HTTP/1.1\r\n" + "Host: localhost:8080\r\n" + "\r\n"
+	req, resp := makeRequest(reqString)
+	if req != nil {
+		t.Error("Expected the server not to receive a request containing invalid Unicode.")
+	}
+
+	badReq := "400 Bad Request"
+	if respString := string(resp); !strings.Contains(respString, badReq) {
+		t.Errorf("Server response: expected it to contain %s, got %s", badReq, respString)
+	}
 
 
 }
 
+// Test behaviour of query parameter parser when passing malformed key or
+// values (by breaking URL encoding). If using Query(), it is supposed to
+// silently discard malformed values.
+func TestQueryParametersBreakUrlEncoding(t *testing.T) {
+	brokenKeyReq := "GET /?vegetable%=tomato HTTP/1.1\r\n" + "Host: localhost:8080\r\n" + "\r\n"
+	req, _ := makeRequest(brokenKeyReq)
+	if lenQueryParams := len(req.URL.Query()); lenQueryParams != 0 {
+		t.Errorf("len(queryParams): got %d, want %d", lenQueryParams, 0)
+	}
 
-// Check whether ParseQuery() gives us all the request
+	brokenValueReq := "GET /?vegetable=tomato% HTTP/1.1\r\n" + "Host: localhost:8080\r\n" + "\r\n"
+	req, _ = makeRequest(brokenValueReq)
+	if lenVeg := len(req.URL.Query()["vegetable"]); lenVeg != 0 {
+		t.Errorf("len(queryParams): got %d, want %d", lenVeg, 0)
+	}
+
+}
+
